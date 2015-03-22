af77fe18b8e76aff41d5e82109bdc2a42dec1ea3
Clinton Wolfe <clinton@omniti.com>
2015-03-22 22:59:45 +0000

    TDD RED - Web app deployment checks

--- cookbooks/widgetworld-app/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/widgetworld-app/test/integration/default/serverspec/default_spec.rb
@@ -4,9 +4,18 @@ describe 'widgetworld-app::default' do
 
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe file('/opt/widgetworld') do
+    it { should be_directory }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+  end 
+
+  describe command('curl http://127.0.0.1') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should_not match(/Apache/) }
   end
 
 end
